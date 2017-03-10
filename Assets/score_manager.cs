9a09333f1b586d15eb55dbd8c15fffc11972f65a
DESKTOP-QVTFV3S\o1834 <ygmdwwyg@gmail.com>
2017-03-10 20:55:20 +0800

    This is my first Unity project uploaded on Github.

---
+++ Assets/score_manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score_manager : MonoBehaviour {
+    private int score_int;
+    private Text score;
+	// Use this for initialization
+	void Start () {
+        score = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void addscore(int num)
+    {
+        score_int += num;
+        score.text = "score: " + score_int.ToString();
+    }
+}
